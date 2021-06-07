8da6e1cfb080c56dc628da08595eb5aede2e3827
Youssef <youssef_a_fadel@hotmail.com>
2021-06-07 17:43:32 +0200

    finalized initial project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
-import uuidv4 from 'uuid/v4'
+import {v4 as uuid} from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
     const [todos, setTodos] = useState([])
     const todoNameRef = useRef()
     const [error, setError] = useState('')
+
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+        if (storedTodos) setTodos(storedTodos)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos])
+
+    function toggleTodo(id) {
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id)
+        todo.complete = !todo.complete
+        setTodos(newTodos)
+    }
 
     function handleAddTodo(e) {
         setError('')
@@ -13,18 +31,23 @@ function App() {
         if (name === '') return setError('Error: Cannot add empty input')
         todoNameRef.current.value = null
         return setTodos(prevTodos => {
-            return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+            return [...prevTodos, {id: uuid(), name: name, complete: false}]
         })
+    }
+
+    function handleClearTodos() {
+        let newTodos = todos.filter(todo => !todo.complete)
+        return setTodos(newTodos)
     }
 
     return (
         <>
             <input ref={todoNameRef} type="text" />
             <button onClick={handleAddTodo}>Add Todo</button>
-            <button>Clear Complete</button>
-            <div>{error}<br/>{todos.length} left to do</div><br />
+            <button onClick={handleClearTodos}>Clear Complete</button>
+            <div>{error}<br/>{todos.filter(todo => !todo.complete).length} left to do</div><br />
             <div></div>
-            <TodoList todos={todos} />
+            <TodoList todos={todos} toggleTodo={toggleTodo} />
         </>
     )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+    }
+
     return (
-        <div><input type="checkbox" checked={todo.complete} /> {todo.name}</div>
+        <div><input type="checkbox" checked={todo.complete} onChange={handleTodoClick} /> {todo.name}</div>
     )
 }
