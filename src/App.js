942b677b71cc7c4a33582597f732bc65cc522d7c
Youssef <youssef_a_fadel@hotmail.com>
2021-06-07 16:42:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([])
+    const todoNameRef = useRef()
+    const [error, setError] = useState('')
+
+    function handleAddTodo(e) {
+        setError('')
+        const name = todoNameRef.current.value
+        if (name === '') return setError('Error: Cannot add empty input')
+        todoNameRef.current.value = null
+        return setTodos(prevTodos => {
+            return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+        })
+    }
+
+    return (
+        <>
+            <input ref={todoNameRef} type="text" />
+            <button onClick={handleAddTodo}>Add Todo</button>
+            <button>Clear Complete</button>
+            <div>{error}<br/>{todos.length} left to do</div><br />
+            <div></div>
+            <TodoList todos={todos} />
+        </>
+    )
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Todo({ todo }) {
+    return (
+        <div><input type="checkbox" checked={todo.complete} /> {todo.name}</div>
+    )
+}
